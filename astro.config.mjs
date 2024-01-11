7e0574744ac7188903ac73d16a77d30b22ac6a90
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2024-01-11 17:05:32 -0600

    todo-comments.nvim integration!

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import svelte from "@astrojs/svelte";
 
 import tailwind from "@astrojs/tailwind";
 
+// TODO: Incorporate middleware for SSR since this is literally just running on your home machine
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,

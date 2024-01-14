06571a3b59aa9b3fc74a713c7ed56ae87fc8cae9
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2024-01-14 13:23:59 -0600

    fake rebase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,16 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-
-// TODO: Incorporate middleware for SSR since this is literally just running on your home machine
-
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: true,
-  outDir: '/var/www/html',
-  integrations: [svelte(), tailwind({
-    applyBaseStyles: false
-  })],
   output: "server",
   adapter: node({
     mode: "standalone"
-  })
+  }),
+  prefetch: true,
+  integrations: [svelte(), tailwind({
+    applyBaseStyles: false
+  })],
 });

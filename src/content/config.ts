c112824ef82cc6d15459966c39b5fb0509dfcb6e
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2024-01-23 15:04:18 -0600

    more icons

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import { z, defineCollection } from 'astro:content'
 
 const bookmarks = defineCollection({
-  type: 'data', // v2.5.0 and later
+  type: 'data',
   schema: z.object({
     url: z.string(),
     icon: z.string(),
--- src/js/data/menu.ts
+++ src/js/data/menu.ts
@@ -1,6 +1,24 @@
+import { getCollection, getEntries } from "astro:content"
+
 export interface MenuItem {
   label: string,
   url: string
+}
+
+let main_menu = async () => {
+  const bookmarks_collection = await getCollection('bookmarks')
+  const bookmarks = await getEntries(bookmarks_collection)
+
+  let menu_items = bookmarks.map((item) => {
+    let slug = item.id.split('/')[0]
+    return {
+      slug: slug
+    }
+  })
+
+  console.log(menu_items)
+
+  return menu_items
 }
 
 export const menu_data: MenuItem[] = [

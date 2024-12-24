e4fb4d5f2e31f1079bddd2c3fcd8babda23d986a
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2024-12-24 16:00:25 -0600

    menu item fixes and hover/active state

--- src/js/data/menu.ts
+++ src/js/data/menu.ts
@@ -1,21 +1,25 @@
-import { getCollection, getEntries } from "astro:content"
+import { getCollection, getEntries } from "astro:content";
 
 export interface MenuItemType {
-  slug: string,
+  label: string;
+  url: string;
 }
 
 let main_menu = async () => {
-  const bookmarks_collection = await getCollection('bookmarks')
-  const bookmarks = await getEntries(bookmarks_collection)
+  const bookmarks_collection = await getCollection("bookmarks");
+  const bookmarks = await getEntries(bookmarks_collection);
 
-  let slugs = ['all', ... new Set(bookmarks.map(({ id }) => id.split('/')[0]))]
+  let slugs = ["all", ...new Set(bookmarks.map(({ id }) => id.split("/")[0]))];
 
   return slugs.map((item_slug) => {
     return {
       label: item_slug,
-      url: `?filter=${item_slug}`
-    }
-  })
-}
+      url: `?filter=${item_slug}`,
+    };
+  });
+};
 
-export const menu_data = await main_menu().then(data => data)
+export const menu_data = await main_menu().then((data) => {
+  console.log(data);
+  return data;
+});
